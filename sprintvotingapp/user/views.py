6eb6eac6eb5d46769545095e779ef6979c719956
KunalHaker <kunal.bathamn@quovantis.com>
2022-05-05 15:00:35 +0530

    Added UC1: User Model is added

--- sprintvotingapp/user/views.py
+++ sprintvotingapp/user/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import ValidationError
 
 
-def index(request):
-    return HttpResponse("Hello")
+class Registration(APIView):
+    def get(self):
+        """
+        Will be Used to Create or Add the User into User Table
+        :return:Response
+        """
+        try:
+            return Response({"message": "User Registration Successfull"})
+        except ValidationError as e:
+            return Response({"message": "User Registration Unsuccessful"})
