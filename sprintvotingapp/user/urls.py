a1d05516d3fb131ae9ca2360c8334160467cb9a2
KunalHaker <kunal.bathamn@quovantis.com>
2022-05-06 13:43:23 +0530

    Updated UC1:User model is implemented Login and Registration working

--- sprintvotingapp/user/urls.py
+++ sprintvotingapp/user/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Registration.as_view(), name='registration'),
+    path('register', views.Registration.as_view(), name='registration'),
+    path('login', views.Login.as_view(), name='login'),
 ]
